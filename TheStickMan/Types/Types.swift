adc06c3af9117e3032a05f59ac56efc97cd5723a
Richard Price <mrspondooli@gmail.com>
2020-09-02 15:16:56 +0100

    added assets, removed boiler plate code

---
+++ TheStickMan/Types/Types.swift
@@ -0,0 +1,13 @@
+//
+//  Types.swift
+//  TheStickMan
+//
+//  Created by Richard Price on 02/09/2020.
+//  Copyright © 2020 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+public let screenWidth: CGFloat = 1536.0
+public let screenHeight: CGFloat = 2048.0
+
